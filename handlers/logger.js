79886cc63576ba3185cafbe7bb493759ff9fdd7f
LaPlace's Demon <164425143+LaPlazesDemon@users.noreply.github.com>
2024-05-25 00:26:08 -0400

    Fixed console.log methods to join ...data array objects

--- handlers/logger.js
+++ handlers/logger.js
@@ -4,12 +4,12 @@ const hide_bin = require('yargs/helpers').hideBin;
 let arguments = yargs(hide_bin(process.argv)).argv;
 
 function log(module_name, ...data) {
-    console.log(`[${module_name}]: `, data);
+    console.log(`[${module_name}]: `, [...data].join(' '));
 }
 
 function debug(module_name, ...data) {
     if (arguments.debug) {
-        console.debug(`[${module_name}] [DEBUG]: `, data);
+        console.debug(`[${module_name}] [DEBUG]: `, [...data].join(' '));
     }
 }
 
